2f5e0ea69f7ae2fa453f1db9238a55b76d6bf54d
poemyear <poemyear.dev@gmail.com>
2017-09-23 00:47:55 -0700

    add basic command

--- lib_hw1/main.c
+++ lib_hw1/main.c
@@ -1,6 +1,74 @@
 #include "main.h"
 
 int main (void) {
+	char cmd[255]={0, };
+	char *ptr;
+	int i;
+	bool isQuit=false;
 
+	while(!isQuit){
+		get_string(cmd);
+		switch (get_command(cmd)) {
+			case QUIT:
+				isQuit=true;
+			break;
+			case CREATE:
+			printf("create\n");
+			break;
+			case DELETE:
+			printf("delete\n");
+			break;
+			case DUMPDATA:
+			printf("dumpadata\n");
+			break;
+			case LIST:
+			printf("list\n");
+			break;
+			case HASH:
+			printf("hash\n");
+			break;
+			case BITMAP:
+			printf("bitmap\n");
+			break;
+			default:
+			break;
+		}
+	}
 	return 0;
+}
+
+void get_string(char *str)
+{	
+	int i;
+	for(i=0;i<100;i++)
+	{
+		scanf("%c",&str[i]);
+		if(str[i] == '\n')
+		{
+			str[i]='\0';
+			break;
+		}
+	}
+}
+
+int get_command(char *cmd) 
+{
+	char * ptr = strtok (cmd, "_ \t");
+	if (cmd[0]=='\0')
+		return -1;
+	if (strcmp(ptr, "quit") == 0)
+		return QUIT;
+	if (strcmp(ptr, "create") == 0)
+		return CREATE;
+	if (strcmp(ptr, "delete") == 0)
+		return DELETE;
+	if (strcmp(ptr, "dumpdata") == 0)
+		return DUMPDATA;
+	if (strcmp(ptr, "list") == 0)
+		return LIST;
+	if (strcmp(ptr, "hash") == 0)
+		return HASH;
+	if (strcmp(ptr, "bitmap") == 0)
+		return BITMAP;
+	return -1;
 }
--- lib_hw1/main.h
+++ lib_hw1/main.h
@@ -1,5 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "list.h"
 #include "hash.h"
 #include "bitmap.h"
+
+#define QUIT                    0
+#define CREATE                  1
+#define DELETE                  2
+#define DUMPDATA                3
+#define LIST                    4
+#define HASH                    5
+#define BITMAP                  6
+
+
+void get_string(char *);
+int get_command();
+
