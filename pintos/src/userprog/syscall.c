83b1254ea1877630a90be42f6af5f83500ddc05d
poemyear <poemyear.dev@gmail.com>
2017-10-13 09:50:05 -0700

    save work - syscall_exit

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -1,5 +1,6 @@
 #include "userprog/syscall.h"
 #include <stdio.h>
+#include <string.h>
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
@@ -10,6 +11,18 @@ void
 syscall_init (void) 
 {
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+}
+
+void
+syscall_exit (int exitstatus) 
+{
+  struct thread *cur = thread_current();
+  char *save_ptr=NULL, *token=NULL;
+  char tmp[129];
+  strlcpy (tmp, cur->name, strlen(cur->name) + 1);
+  token = strtok_r (tmp, " ", &save_ptr);
+  printf ("%s: exit(%d)\n", token, exitstatus);
+  thread_exit ();
 }
 
 static void
