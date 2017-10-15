8a971a8265a7f0c00a9f841921e2fa1f97a14b5b
poemyear <poemyear.dev@gmail.com>
2017-10-15 01:35:29 -0700

    add thread pointer to parent thread

--- pintos/src/threads/thread.h
+++ pintos/src/threads/thread.h
@@ -90,6 +90,8 @@ struct thread
     int priority;                       /* Priority. */
     struct list_elem allelem;           /* List element for all threads list. */
 
+	struct thread *parent;				/* Parent pointer. */
+
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
 
